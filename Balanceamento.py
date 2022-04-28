4547229219f4e31e52e720e20ce3e7f3f34e3024
andre.freddi <52337680+andrefreddi@users.noreply.github.com>
2022-04-27 23:42:24 -0300

    Balanceamento

---
+++ Balanceamento.py
@@ -0,0 +1,31 @@
+from pythonds.basic.stack import Stack
+
+def VerificarPar(ParentesesString):
+    Pilha = Stack()
+    Equilibra = True
+    indice = 0
+    while indice < len(ParentesesString) and Equilibra:
+        Parenteses = ParentesesString[indice]
+        if Parenteses == "{" or Parenteses == "(" or Parenteses == "[":
+            Pilha.push(Parenteses)
+        else:
+            if Pilha.isEmpty():
+                Equilibra = False
+            else:
+                Pilha.pop()
+
+        indice = indice + 1
+
+    if Equilibra and Pilha.isEmpty():
+        return True
+    else:
+        return False
+
+print(VerificarPar('((()))')) # True
+print(VerificarPar('(())')) # True
+print(VerificarPar('()())')) # false
+
+#Referente ao exercicio do hackerrank -->  {[()]} -> balanceado {[(])} -> não balanceado {{[[(())]]}} -> balanceado
+print(VerificarPar('{[()]}')) # True
+print(VerificarPar('{[()[]}')) # false
+print(VerificarPar('{{[[(())]]}}{')) # false
